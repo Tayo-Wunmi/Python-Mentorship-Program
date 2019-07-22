8ce0d99bb27b2110a7fff50251590b2fb7c00b13
Temitayo Ajakore <49158747+Tayo-Wunmi@users.noreply.github.com>
2019-07-22 14:43:50 +0100

    Add files via upload

---
+++ WEEK ONE/Challenge 1, Even Or Odd.py
@@ -0,0 +1,13 @@
+print("Welcome to the number identificator. Enjoy and have fun!")
+
+fav_num = int(input("Type your favourite number: "))
+
+if fav_num % 2 == 0:
+    print('Your number favourite is an even number')
+
+else:
+    print('Your number favourite is an odd number')
+
+print("Thank you for playing the number identificator. Hope you had fun! "
+      "\n"
+      "Please give comment or feedback: ")
