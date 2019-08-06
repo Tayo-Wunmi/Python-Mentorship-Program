fce36541474a6a525aeefe1a7c1b9bf8e2bf319d
Temitayo Ajakore <49158747+Tayo-Wunmi@users.noreply.github.com>
2019-08-06 07:57:11 +0100

    Update Week 2, Challenge 3.py

--- WEEK 2/Week 2, Challenge 3.py
+++ WEEK 2/Week 2, Challenge 3.py
@@ -9,12 +9,3 @@
 # 2. FOR LOOP
 
 
-
-#Write a program that simulates rolling a die. When the program runs, it will randomly choose a number between 1 and 6 and print,
-#then ask the user if they will like to roll again.
-
-print("Welcome to the Dicing Game. Lets roll!")
-
-for i in range(1,7):
-    print(i)
-    print("Roll your dice again")
