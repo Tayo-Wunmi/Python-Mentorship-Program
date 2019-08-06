4e4f8a86389fddbb88df6f976a52312b198555cf
Temitayo Ajakore <49158747+Tayo-Wunmi@users.noreply.github.com>
2019-08-06 07:58:53 +0100

    Update Week 2, Challenge 4.py

--- WEEK 2/Week 2, Challenge 4.py
+++ WEEK 2/Week 2, Challenge 4.py
@@ -1,9 +1,10 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
+
+# CHALLENGE 
 #Write a program that simulates rolling a die. When the program runs, it will randomly choose a number between 1 and 6 and print,
 #then ask the user if they will like to roll again.
 
+
+# SOLUTION 
 print("Welcome to the Dicing Game. Lets roll!")
 dice = input("Will you like to Roll your dice:  ")
 
@@ -24,5 +25,5 @@ while dice=="yes" or roll_again=="yes":
 
     break
 
-
 roll_again = input("Will you like to Roll your dice again:  ")
+
